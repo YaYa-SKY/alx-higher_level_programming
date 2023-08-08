bdc98c051a558e594ac9e35a2b80dca2e6d18162
YaYa-SKY <yahia.seqqai@gmail.com>
2023-08-08 11:49:00 -0700

    pycodestyle error

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
- for number in range(1, 101):
+    for number in range(1, 101):
         if number % 3 == 0 and number % 5 == 0:
             print("FizzBuzz ", end="")
         elif number % 3 == 0:
@@ -9,4 +9,3 @@ def fizzbuzz():
             print("Buzz ", end="")
         else:
             print("{} ".format(number), end="")
-
