1beb5fb2580d706a51e73425e1b64665f538e356
YaYa-SKY <yahia.seqqai@gmail.com>
2023-08-10 05:15:51 -0700

    done

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b, c):
+    """Custom bytecode pattern inspired by educational examples."""
+    if a < b:
+        return c
+    if c > b:
+        return a + b
+    return a * b - c
