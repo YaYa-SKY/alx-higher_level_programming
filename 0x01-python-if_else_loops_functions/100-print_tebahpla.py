70a3ad0e6c9aea04a597307d7805e7c3a26fc1d1
YaYa-SKY <yahia.seqqai@gmail.com>
2023-08-10 04:51:24 -0700

    new update

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
+output = ""
 for i in range(25, -1, -1):
-    print(chr(122 - i), end='')
-    if i % 2 == 0:
-        print(chr(89 - i), end='')
+    output += "{}{}".format(chr(122 - i), chr(89 - i)) if i % 2 == 0 else chr(122 - i)
+print(output)
