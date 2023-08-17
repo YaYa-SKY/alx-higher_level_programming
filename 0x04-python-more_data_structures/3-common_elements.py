ec5fdd282a0b920b0f989df9a467a0384a66fc86
YaYa-SKY <yahia.seqqai@gmail.com>
2023-08-17 00:43:44 -0700

    common elements editted

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-set1 = {1, 2, 3, 4, 5}
-set2 = {3, 4, 5, 6, 7}
-result = common_elements(set1, set2)
-print(result)
+
+
+def common_elements(set_1, set_2):
+    """
+    A function that returns a set of
+    common elements in two sets
+    """
+    return(set_1 & set_2)
