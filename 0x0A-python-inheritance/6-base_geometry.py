0faff6ca412eb25b4caa301cc9261493d335a637
YaYa-SKY <yahia.seqqai@gmail.com>
2023-09-12 02:04:18 -0700

    new update

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define base geometry class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Representing base geometry."""
+
+    def area(self):
+        """Is not implemented"""
+        raise Exception("area() is not implemented")
