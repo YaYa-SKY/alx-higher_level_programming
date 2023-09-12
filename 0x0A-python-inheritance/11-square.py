ccc4a02427242a2ea5227a728082cfe2e72dcffd
YaYa-SKY <yahia.seqqai@gmail.com>
2023-09-12 02:17:49 -0700

    last update

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 Rectangle = __import__('9-rectangle').Rectangle
-"""
-===================================
-module with class BaseGeometry
-===================================
-"""
-
 
 class Square(Rectangle):
     """Square class that inherits from Rectangle that inherits BaseGeometry."""
