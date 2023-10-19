83fd9eb161b228f138a7fdb0888695e8334532e2
YaYa-SKY <yahia.seqqai@gmail.com>
2023-10-19 07:45:19 -0700

    updated

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,7 +1,7 @@
--- Retrieves a list of TV shows and their linked genres from the hbtn_0d_tvshows database.
--- Utilizes SQL to extract data from tables that are linked together and orders the results by show title and genre name.
-SELECT tv_shows.title AS show_title, tv_genres.name AS genre_name
+-- lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
+-- lists all rows of a table linked to another table
+SELECT title, name
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY show_title ASC, genre_name ASC;
+ORDER BY title ASC, name ASC;
