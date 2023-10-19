0c335b9480cffa4dba27a4356bfc8b3f3b6d96db
YaYa-SKY <yahia.seqqai@gmail.com>
2023-10-19 06:46:56 -0700

    updated

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,4 @@
+-- Establishes the 'force_name' table on your MySQL server.
+-- Table creation within a database:
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
+
---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,4 @@
+-- Establishes the 'id_not_null' table on your MySQL server.
+-- Table creation:
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
+
